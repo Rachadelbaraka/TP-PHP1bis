8f8906c3d7bc16b49f97fb7f4fdcc07b1f013e2f
Your Name <you@example.com>
2025-04-08 09:42:41 +0200

    Exercices boucle for

---
+++ index.php
@@ -0,0 +1,24 @@
+
+<?php
+// TP PHP 1 // 
+
+//bouclefor// 
+
+echo "<h3>Exercice 1 : Afficher les nombres de 1 à 10</h3>";
+for ($i = 1; $i <= 10; $i++) {
+    echo $i . " ";
+}
+
+echo "<h3>Exercice 2 : Somme des nombres de 1 à 100</h3>";
+$sum = 0;
+for ($i = 1; $i <= 100; $i++) {
+    $sum += $i;
+}
+echo "La somme est : $sum";
+
+   echo "<h3>Exercice 3 : Table de multiplication de 5</h3>";
+$nombre = 5;
+for ($i = 1; $i <= 10; $i++) {
+    echo "$nombre x $i = " . ($nombre * $i) . "<br>";
+}
+
