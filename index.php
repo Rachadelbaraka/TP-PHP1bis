bb8068f0a1cb20ab608012785e4b2eb9c9e96042
Your Name <you@example.com>
2025-04-08 10:55:35 +0200

    switch

--- index.php
+++ index.php
@@ -2,24 +2,17 @@
 <?php
 // TP PHP 1 // 
 
-// Boucle for //
+// Switch
 
-// Exercice 1 //
+echo "<h3>Exercice 1 : Message selon le jour</h3>";
+$jour = date('l');
+$message = "";
 
-echo "<h3>Exercice 1 : Afficher les nombres de 1 à 10</h3>";
-for ($i = 1; $i <= 10; $i++) {
-    echo $i . " ";
+switch ($jour) {
+    case 'Monday': $message = "Courage, c'est lundi !"; break;
+    case 'Friday': $message = "C'est bientôt le week-end !"; break;
+    case 'Sunday': $message = "Repose-toi bien aujourd'hui."; break;
+    default: $message = "Bonne journée !"; break;
 }
+echo "Aujourd'hui c'est $jour : $message<br><br>";
 
-echo "<h3>Exercice 2 : Somme des nombres de 1 à 100</h3>";
-$sum = 0;
-for ($i = 1; $i <= 100; $i++) {
-    $sum += $i;
-}
-echo "La somme est : $sum";
-
-   echo "<h3>Exercice 3 : Table de multiplication de 5</h3>";
-$nombre = 5;
-for ($i = 1; $i <= 10; $i++) {
-    echo "$nombre x $i = " . ($nombre * $i) . "<br>";
-}
