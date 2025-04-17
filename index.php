d719f0f014c36a2ef9a65d3a0c2a0a041f4f89f7
Your Name <you@example.com>
2025-04-17 15:30:51 +0200

    Vehicules

--- index.php
+++ index.php
@@ -1,28 +1,80 @@
 <?php
-require_once 'Vehicule.php';
+
+class Vehicule {
+    protected $marque;
+    protected $modele;
+    protected $annee;
+    
+    public function __construct($marque, $modele, $annee) {
+        $this->marque = $marque;
+        $this->modele = $modele;
+        $this->annee = $annee;
+    }
+    
+    public function getInfos() {
+        return "Marque : $this->marque, Modèle : $this->modele, Année : $this->annee";
+    }
+    
+    public function demarrer() {
+        return "Le véhicule démarre.";
+    }
+}
+
 
 class Voiture extends Vehicule {
     private $nombrePortes;
     private $typeCarburant;
-
+    
     public function __construct($marque, $modele, $annee, $nombrePortes, $typeCarburant) {
+     
         parent::__construct($marque, $modele, $annee);
+        
+       
         $this->nombrePortes = $nombrePortes;
         $this->typeCarburant = $typeCarburant;
     }
-
+    
+  
     public function getInfos() {
-        return parent::getInfos() . " - $this->nombrePortes portes - $this->typeCarburant";
+        return parent::getInfos() . ", Portes : $this->nombrePortes, Carburant : $this->typeCarburant";
     }
-
+    
+   
     public function klaxonner() {
-        return "Tut tut !";
+        return "Bip Bip !";
     }
 }
 
-// Exemple
-$v = new Voiture("Peugeot", "208", 2022, 5, "Essence");
-echo $v->getInfos() . "<br>";
-echo $v->demarrer() . "<br>";
-echo $v->klaxonner();
-?>
+
+class Moto extends Vehicule {
+    private $cylindree;
+    
+    public function __construct($marque, $modele, $annee, $cylindree) {
+        parent::__construct($marque, $modele, $annee);
+        $this->cylindree = $cylindree;
+    }
+
+
+    public function getInfos() {
+        return parent::getInfos() . ", Cylindrée : $this->cylindree cc";
+    }
+
+    public function faireWheelie() {
+        return "La moto fait un wheeling !";
+    }
+}
+
+
+$voiture = new Voiture("Renault", "Clio", 2020, 5, "Essence");
+$moto = new Moto("Yamaha", "MT-07", 2022, 689);
+
+
+echo "<h2>Voiture</h2>";
+echo $voiture->getInfos() . "<br>";
+echo $voiture->demarrer() . "<br>";
+echo $voiture->klaxonner() . "<br><br>";
+
+echo "<h2>Moto</h2>";
+echo $moto->getInfos() . "<br>";
+echo $moto->demarrer() . "<br>";
+echo $moto->faireWheelie();
